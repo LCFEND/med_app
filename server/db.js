fc3405f431f0e0f324f73c21e34df6544ae713e5
lcfend <leecoombes@mail.de>
2025-01-10 09:21:18 -0500

    Mongo

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI =  "mongodb://root:<your-password>@127.0.0.1:27017";
+const mongoURI =  "mongodb://root:kOG4uYqR4VWHKhLed859NV2E@172.21.113.100:27017";
 
 const connectToMongo = async (retryCount) => {
     const MAX_RETRIES = 3;

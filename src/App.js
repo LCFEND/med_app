8ea01423e06ca0a85bbb2e954c126d0dca4b43e2
lcfend <leecoombes@mail.de>
2025-01-13 04:38:25 -0500

    Landing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom"; // Make sure Route is imported
+import Navbar from './Components/Navbar/Navbar';
 import './App.css';
+import Landing_Page from './Components/Landing_Page/Landing_Page'; // Correct path and PascalCase
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        {/* Set up BrowserRouter for routing */}
+        <BrowserRouter>
+          {/* Display the Navbar component */}
+          <Navbar />
+          {/* Set up the Routes for different pages */}
+          <Routes>
+            {/* Define Route for the landing page */}
+            <Route path="/" element={<Landing_Page />} /> {/* Correct usage of Landing_Page component */}
+          </Routes>
+        </BrowserRouter>
     </div>
   );
 }
 
+// Export the App component as the default export
 export default App;
